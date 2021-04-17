e41c5cb802b4fa022228dae718743357220e559c
Phạm Minh Tiến <phaminhtien2424@gmail.com>
2021-04-17 12:26:43 +0700

    (fix): search bar styles

--- src/packages/base/SearchBar/SearchBar.tsx
+++ src/packages/base/SearchBar/SearchBar.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 const SearchBar = () => {
   return (
     <>
-      <div className='wrap'>
-        <div className='search'>
-          <input type='text' className='searchTerm' placeholder='Search' />
-          <button type='submit' className='searchButton'>
-            {/* with icon here */}
-          </button>
-        </div>
+      <div className='input-group'>
+        <input
+          className='search'
+          type='text'
+          placeholder='Searching for something?'
+        />
+        <span className='bar'></span>
       </div>
     </>
   );

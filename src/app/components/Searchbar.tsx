fae7c3511d835bb0c58cc6dacd3ce2eec87e52b4
Tugay Serinken <tugay.serinken@hotmail.de>
2021-04-18 06:47:52 +0700

    add searchbar and sidebar

--- src/app/components/Searchbar.tsx
+++ src/app/components/Searchbar.tsx
@@ -1,3 +1,4 @@
+import './SearchBar.sass'
 import React from 'react'
 
 type SearchbarProps = {
@@ -5,12 +6,26 @@ type SearchbarProps = {
   setInputText: React.Dispatch<React.SetStateAction<string>>
 }
 
-export const Searchbar = ({inputText, setInputText}: SearchbarProps) => {
+export const SearchBar = ({inputText, setInputText}: SearchbarProps) => {
   return (
-      <input
-        value={inputText}
-        onChange={e => setInputText(e.target.value)}
-        placeholder="Search products"
-      />
+    <div className='input-group'>
+      <input className='search' type='text' value={inputText} onChange={e => setInputText(e.target.value)}
+      placeholder="Search products" />
+      <button
+        className='pre-search-btn ripple'
+        data-var='vsButton'
+        aria-label='Search'
+      >
+        <svg
+          className='pre-search-input-icon'
+          fill='#111'
+          height='30px'
+          width='30px'
+          viewBox='0 0 24 24'
+        >
+          <path d='M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.39zM11 18a7 7 0 1 1 7-7 7 7 0 0 1-7 7z'></path>
+        </svg>
+      </button>
+    </div>
   )
 }
---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import './Sidebar.sass';
+import 'react-input-range/lib/css/index.css';
+
+import React, { useState } from 'react';
+
+import InputRange from 'react-input-range';
+
+const Sidebar: React.FC = () => {
+  const [value, setValue] = useState({ min: 250, max: 800 });
+
+  return (
+    <>
+      <div className='sidebar'>
+        <h3>FILTER SELECTION</h3>
+        <span className='label'>PRICE</span>
+        <div className='slider'>
+          <InputRange
+            maxValue={1000}
+            minValue={0}
+            formatLabel={(value) => `${value} $`}
+            value={value}
+            onChange={(value) => setValue(value)}
+          />
+        </div>
+        <span className='range'>
+          Range: ${value.min} - ${value.max}
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
